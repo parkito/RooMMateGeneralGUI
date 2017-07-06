a064b7d605337db04f1429fb1d688e5cbb615d96
parkito <artyom-karnov@yandex.ru>
2017-07-06 22:15:10 +0300

    Add service pages

---
+++ controller/src/main/java/com/roommate/generalgui/controller/controllers/ServiceControllers.java
@@ -0,0 +1,9 @@
+package com.roommate.generalgui.controller.controllers;
+
+/**
+ * @author Artem Karnov @date 06.07.2017.
+ *         artem.karnov@t-systems.com
+ */
+
+public class ServiceControllers {
+}
