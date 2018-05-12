0a89e1c07b95ed8dd305b45ec00c262ccc681843
parkito <artyom-karnov@yandex.ru>
2018-05-12 16:59:54 +0300

    Add db programatic copy

--- src/main/java/ru/siksmfp/spring/annotation/conf/DBConfig.java
+++ src/main/java/ru/siksmfp/spring/annotation/conf/DBConfig.java
@@ -1,19 +1,29 @@
 package ru.siksmfp.spring.annotation.conf;
 
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
-import java.io.IOException;
 import java.util.Properties;
+
+import static org.hibernate.cfg.AvailableSettings.C3P0_ACQUIRE_INCREMENT;
+import static org.hibernate.cfg.AvailableSettings.C3P0_MAX_SIZE;
+import static org.hibernate.cfg.AvailableSettings.C3P0_MAX_STATEMENTS;
+import static org.hibernate.cfg.AvailableSettings.C3P0_MIN_SIZE;
+import static org.hibernate.cfg.AvailableSettings.C3P0_TIMEOUT;
+import static org.hibernate.cfg.AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS;
+import static org.hibernate.cfg.AvailableSettings.DIALECT;
+import static org.hibernate.cfg.AvailableSettings.DRIVER;
+import static org.hibernate.cfg.AvailableSettings.HBM2DDL_AUTO;
+import static org.hibernate.cfg.AvailableSettings.PASS;
+import static org.hibernate.cfg.AvailableSettings.SHOW_SQL;
+import static org.hibernate.cfg.AvailableSettings.URL;
+import static org.hibernate.cfg.AvailableSettings.USER;
 
 /**
  * @author Artem Karnov @date 5/11/2018.
@@ -23,48 +33,43 @@ import java.util.Properties;
 @EnableTransactionManagement
 @PropertySource("classpath:database.properties")
 public class DBConfig {
+
     @Autowired
     private Environment env;
 
     @Bean
-    public HibernateTemplate hibernateTemplate() {
-        return new HibernateTemplate(sessionFactory());
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+
+        Properties props = new Properties();
+        // Setting JDBC properties
+        props.put(DRIVER, env.getProperty("database.driver"));
+        props.put(URL, env.getProperty("database.url"));
+        props.put(USER, env.getProperty("database.user"));
+        props.put(PASS, env.getProperty("database.password"));
+
+        // Setting Hibernate properties
+        props.put(DIALECT, env.getProperty("hibernate.dialect"));
+        props.put(CURRENT_SESSION_CONTEXT_CLASS, env.getProperty("hibernate.current_session_context_class"));
+        props.put(SHOW_SQL, env.getProperty("hibernate.show_sql"));
+        props.put(HBM2DDL_AUTO, env.getProperty("hibernate.hbm2ddl.auto"));
+
+
+        // Setting C3P0 properties
+        props.put(C3P0_MIN_SIZE, env.getProperty("hibernate.c3p0.min_size"));
+        props.put(C3P0_MAX_SIZE, env.getProperty("hibernate.c3p0.max_size"));
+        props.put(C3P0_ACQUIRE_INCREMENT, env.getProperty("hibernate.c3p0.acquire_increment"));
+        props.put(C3P0_TIMEOUT, env.getProperty("hibernate.c3p0.timeout"));
+        props.put(C3P0_MAX_STATEMENTS, env.getProperty("hibernate.c3p0.max_statements"));
+
+        factoryBean.setHibernateProperties(props);
+        return factoryBean;
     }
 
     @Bean
-    public SessionFactory sessionFactory() {
-        LocalSessionFactoryBean lsfb = new LocalSessionFactoryBean();
-        lsfb.setDataSource(getDataSource());
-        lsfb.setPackagesToScan("com.concretepage.entity");
-        lsfb.setHibernateProperties(hibernateProperties());
-        try {
-            lsfb.afterPropertiesSet();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return lsfb.getObject();
-    }
-
-    @Bean
-    public DataSource getDataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(env.getProperty("database.driver"));
-        dataSource.setUrl(env.getProperty("database.url"));
-        dataSource.setUsername(env.getProperty("database.root"));
-        dataSource.setPassword(env.getProperty("database.password"));
-        return dataSource;
-    }
-
-    @Bean
-    public HibernateTransactionManager hibTransMan() {
-        return new HibernateTransactionManager(sessionFactory());
-    }
-
-    private Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
-        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
-        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-        return properties;
+    public HibernateTransactionManager getTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(getSessionFactory().getObject());
+        return transactionManager;
     }
 }
